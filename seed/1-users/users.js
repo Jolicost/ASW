1e98d924209707ddf699a21ecca71e9d65f6c449
Joan Francesc Burguera Burguera <37141963+joaaan9@users.noreply.github.com>
2018-11-18 16:18:19 +0100

    Add files via upload

--- seed/1-users/users.js
+++ seed/1-users/users.js
@@ -1,5 +1,5 @@
 const { getObjectId } = require('../index');
-
+// date = yyyy-mm-dd
 const users = [
   {
     _id: getObjectId('Federico'),
@@ -14,16 +14,94 @@ const users = [
     createdAt: '1890-01-01'
   },
   {
-    _id: getObjectId('Napoleon'),
-    username: 'Napoleon',
-    password: 'prussia',
-    createdAt: '1790-01-01'
+    _id: getObjectId('Junqueras'),
+    username: 'OfficialOriol',
+    password: 'puigdebobo',
+    createdAt: '2015-12-31'
+  },
+  {
+    _id: getObjectId('Rivera'),
+    username: 'Albert_Rivera',
+    password: 'primoderivera',
+    createdAt: '2001-01-20'
+  },
+  {
+    _id: getObjectId('Sanchez'),
+    username: 'Sanchez_Castejon',
+    password: 'pabloiglesias',
+    createdAt: '2006-07-11'
+  },
+  {
+    _id: getObjectId('Aznar'),
+    username: 'JoseMariaAznar',
+    password: 'valledeloscaidos',
+    createdAt: '2010-09-01'
+  },
+  {
+    _id: getObjectId('Sebastian'),
+    username: 'SebOfficial',
+    password: 'naitsabes',
+    createdAt: '2018-10-10'
+  },
+  {
+    _id: getObjectId('Casado'),
+    username: 'Casado_Pablo',
+    password: 'unidad',
+    createdAt: '2015-04-02'
+  },
+  {
+    _id: getObjectId('Rubalcaba'),
+    username: 'Rubalcalvo',
+    password: 'socialismo',
+    createdAt: '2014-07-01'
+  },
+  {
+    _id: getObjectId('Valtonyc'),
+    username: 'JosepArenas',
+    password: 'injurias',
+    createdAt: '1999-09-19'
+  },
+  {
+    _id: getObjectId('Echenique'),
+    username: 'Echenako',
+    password: 'irenemontero',
+    createdAt: '2000-04-13'
+  },
+  {
+    _id: getObjectId('Puigdemont'),
+    username: 'Puigdemont',
+    password: 'catalunya',
+    createdAt: '2006-02-23'
+  },
+  {
+    _id: getObjectId('Wyoming'),
+    username: 'ElIntermedio',
+    password: 'lasexta',
+    createdAt: '2011-05-30'
+  },
+  {
+    _id: getObjectId('LeBron'),
+    username: 'jamesLeb',
+    password: 'kobebryant',
+    createdAt: '1964-11-21'
+  },
+  {
+    _id: getObjectId('Curry'),
+    username: 'StephenCurry',
+    password: 'kevinDurant',
+    createdAt: '1977-12-22'
+  },
+  {
+    _id: getObjectId('Westbrook'),
+    username: 'Russellw',
+    password: 'burger',
+    createdAt: '2018-01-29'
   },
   {
     _id: getObjectId('Llarena'),
     username: 'Juez_Llarena',
     password: 'extradicion',
-    createdAt: '2005-01-01'
+    createdAt: '2005-01-11'
   },
   {
     _id: getObjectId('Trump'),
